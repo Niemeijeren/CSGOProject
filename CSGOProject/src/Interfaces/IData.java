a8beaf4836b694a03fb594d8e7603c050a4d6459
Niemeijeren <niemeijeren@gmail.com>
2018-03-07 23:12:51 +0100

    Første metode
    Første metode for at finde Coaches og holdet de er del af

--- CSGOProject/src/Interfaces/IData.java
+++ CSGOProject/src/Interfaces/IData.java
@@ -1,10 +1,14 @@
 
 package Interfaces;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author niemeijeren
  */
 public interface IData {
     
+    public ArrayList<String> getData(String query);
+    public ArrayList<String> getCoachInfo();
 }
--- CSGOProject/src/data/Data.java
+++ CSGOProject/src/data/Data.java
@@ -1,6 +1,7 @@
 package data;
 import Interfaces.IData;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 /*
 	Run with:
@@ -17,33 +18,43 @@ public class Data implements IData
     private String url = "jdbc:postgresql://horton.elephantsql.com:5432/sywigydc";
     private String username = "sywigydc";
     private String password = "U61YX4R8TCresQKKjJvTpcot4dQubxdV";
-    private String[] data;
+    private ArrayList<String> data;
+    Connection db;
+        
     
     public Data()
             
     {
-        // Hver gang vi opretter et objekt af vores data-lag, vil vi have at den opretter
-        //forbindelse til vores DB som det første via try/catch.
+        
+        //Tjek at vi har vores SQL driver
         try {
             Class.forName("org.postgresql.Driver");
         } catch (java.lang.ClassNotFoundException e) {
             System.out.println(e);
         }
         
+        // Hver gang vi opretter et objekt af vores data-lag, vil vi have at den opretter
+        //forbindelse til vores DB som det første via try/catch.
+        try {
+            db = DriverManager.getConnection(url, username, password);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
     
     
- public String[] getSomething()
+    //Methode til at hente data fra database i StringArray
+    
+ public ArrayList<String> getData(String query)
  {
         try {
-            Connection db = DriverManager.getConnection(url, username, password);
-
             Statement st = db.createStatement();
             ResultSet rs = st.executeQuery("SELECT * FROM Persons");
             while (rs.next()) {
-
-                System.out.print(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4) + "\n");
-
+                int i = 1;
+                while (rs.getString(i) != null){
+                    System.out.print(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4) + "\n");
+                }
                 // push test
             }
             rs.close();
@@ -56,6 +67,36 @@ public class Data implements IData
         
         return data;
         
+    }
+
+    @Override
+    public ArrayList<String> getCoachInfo() {
+            try {
+                Statement st = db.createStatement();
+                
+                // Vælg/Vis Persons.navn, Persons.nickname, part_of.teamname
+                // Fra Persons Natural join med part_of 
+                // (Flet sammen med fælles kolonne, og vis de valgte kolonner)
+                //Hvor Persons.role = 'coach'
+                ResultSet rs = st.executeQuery("SELECT Persons.name, Persons.nickname, part_of.teamname "
+                                            + "FROM Persons NATURAL JOIN part_of "
+                                            + "WHERE Persons.role = 'coach'");
+            while (rs.next()) {
+                int i = 1;
+                while (rs.getString(i) != null){
+                    System.out.print(rs.getString(1) + "\t" + rs.getString(2) + "\t" + rs.getString(3) + "\t" + rs.getString(4) + "\n");
+                }
+                // push test
+            }
+            rs.close();
+            st.close();
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        return data;    
     }
 
 
